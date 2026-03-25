c180696dd06e15879e53e8dc010e03e6d670536d
Aboogeeky <Idrishassan35@gmail.com>
2026-03-25 12:47:53 +0100

    feat: Semgrep + Secret Scanning CI Pipeline (#<issue number>) (#176)
    * feat: implement Firebase App Check for API protection (#62)
    
    * feat: add Semgrep + secret scanning CI pipeline for security
    
    ---------
    
    Co-authored-by: Hahfyeez <36053600+Hahfyeex@users.noreply.github.com>

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,6 +1,5 @@
 require("dotenv").config();
 const express = require("express");
-const http = require("http");
 const cors = require("cors");
 const crypto = require("crypto");
 const logger = require("./utils/logger");
